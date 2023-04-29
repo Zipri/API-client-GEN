92e765418dae3f4e1f14f0e976f34b54c4887b25
kolosovskiy_ma <kolosovskiy_ma@rtsoft.ru>
2023-04-29 19:41:48 +0300

    delete deploy

--- server/scripts/pre_deploy.js
+++
@@ -1,9 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const fs = require('fs');
-
-const generate = () => {
-  fs.copyFileSync('./package.json', '../dist/server/package.json');
-  fs.copyFileSync('./.htaccess', '../dist/.htaccess');
-};
-
-generate();
